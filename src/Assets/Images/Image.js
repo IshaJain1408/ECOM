00a20995f11b18af4354d79b545c65955a9c4620
IshaJain1408 <116328120+IshaJain1408@users.noreply.github.com>
2024-02-06 11:28:14 +0530

    Created online shopping Website

---
+++ src/Assets/Images/Image.js
@@ -0,0 +1,11 @@
+export let payment = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838910/payment_mkel3w.jpg';
+export let profile = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838912/vecteezy_user-avatar-line-style__x94mjc.jpg';
+export let MaleCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Male-Cloth_fyyrzb.jpg';
+export let FemaleCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838912/Cloths_kpwmp2.jpg';
+export let GroupCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Cloth-banner_ylavk9.jpg';
+export let Electronics = 'https://images.pexels.com/photos/10799530/pexels-photo-10799530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+export let Jewelry = 'https://images.pexels.com/photos/3387577/pexels-photo-3387577.jpeg';
+export let Shoes = 'https://images.pexels.com/photos/260044/pexels-photo-260044.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+export let Books = 'https://images.pexels.com/photos/4273468/pexels-photo-4273468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+export let EmptyCart = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838909/Cart_bk4xgl.jpg';
+export let customerReview = "https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Review_bm6chw.jpg";
